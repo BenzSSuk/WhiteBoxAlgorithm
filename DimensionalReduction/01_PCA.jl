a942fbc06fe8d3a54bae1701d4a1a6d6f5f41139
BenzSSuk <Pannawit.ssuk@outlook.com>
2020-12-20 19:41:59 +0700

    Complete example and change the way to import zTools

--- DimensionalReduction/01_PCA.jl
+++ DimensionalReduction/01_PCA.jl
@@ -2,28 +2,31 @@
 # Standard
 using CSV
 using DataFrames
-
-
-
+using Statistics
+using Plots
 # custom
 # find current dirctory
 # if open folder in vscode, pwd() = open folder
 currentDir=pwd()
 projDir=currentDir;
+
+print(["Current Dir: " projDir])
+
 # this included file may be contain with multiple module 
 include(joinpath(projDir,raw"DimensionalReduction\lib\DimensionalReduction.jl"))
-import .DimensionalReduction # import module in file that already included
+import .DimReduce # import module in file that already included
 
-include(joinpath(projDir,raw"Tools\FileAndTable.jl"))
-import .FileAndTable
-
+# include(joinpath(projDir,raw"Tools\FileAndTable.jl"))
+include(joinpath(projDir,raw"Tools\zTools.jl"))
+import .zTools
 
 ## Load feature
 pathFeat=raw"C:\Users\Panna\OneDrive\rawdata\20200502";
-featureTable,listFeat=FileAndTable.LoadTable(pathFeat,".csv",0);
+listFile=zTools.GetList(pathFeat)
+featureTable,listFeat=zTools.LoadTable(pathFeat,".csv",0);
 
 ## select feature
-headerFeature=names(featureTable[2]);
+headerFeature=names(featureTable[4]);
 listFeatureSelect=["RT_90s_SD", "RT_90s_mean",
        "RT_90s_SDPP", "RT_90s_meanPP", "RT_90s_HRV_sm_RMSSD",
        "RT_90s_HRV_smnm_SD", "RT_90s_HRV_sm_nm_SD", "RT_90s_meanPI",
@@ -33,21 +36,19 @@ listFeatureSelect=["RT_90s_SD", "RT_90s_mean",
        "featHRV_90s_LFnu2", "featHRV_90s_HF", "featHRV_90s_HFnu",
        "featHRV_90s_HFnu2", "featHRV_90s_conv_HF", "featHRV_90s_LFHF"];
 
+# select only column in DF that is feature value and convert to matric 
+featureMat=Matrix(featureTable[4][:,listFeatureSelect]);
+# featureMat=convert(Matrix,featureTable[4][:,listFeatureSelect]);
+featureMat,indexFilOut=zTools.FilterDF(featureMat,["nan"]);
+
 ## PCA
-using Statistics
-a=[1 1 1;
-   2 4 6];
-aMean=mean(a,dims=1)
-print(aMean)
-a.-aMean
+newdims=3;
+featureMatNew,eigRatio,covMat_eigVal, covMat_eigVec=DimReduce.PCA(featureMat,newdims);
 
-## cov
-aCov=cov(a)
+gr()
+# plot(featureMatNew[:,1],featureMatNew[:,2],featureMatNew[:,3])
+scatter(featureMatNew[:,1],featureMatNew[:,2],featureMatNew[:,3])
 
-## 
-using LinearAlgebra
-eigvals(aCov)
-eigvecs(aCov)
 
 
 
